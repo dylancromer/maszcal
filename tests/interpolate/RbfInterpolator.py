ae0ff8e77d68eb61926265cc049c6b321bf12212
dylancromer <dylanmattc@gmail.com>
2019-05-31 12:37:46 -0400

    Updated GP test and added RBF test

--- tests/interpolate/RbfInterpolator.py
+++ tests/interpolate/RbfInterpolator.py
@@ -1,36 +1,36 @@
 """
-GaussInterpolator
+RbfInterpolator
 
 Interpolates maszcal model grids
 """
 import numpy as np
-from maszcal.interpolate import GaussInterpolator
+from maszcal.interpolate import RbfInterpolator
 
 
 
 
-length = 3
+length = 5
 xs = np.linspace(0, 3, length)
 ys = np.linspace(0, 3, length)
 zs = np.linspace(0, 3, length)
 coords = (xs, ys, zs)
 grid = np.ones((length, length, length))
 
-gauss_interpolator = GaussInterpolator(coords, grid)
+interpolator = RbfInterpolator(coords, grid)
 
 
 """
     - it interpolates a constant to be a constant
 """
 def test_interp_over_constant_input():
-    gauss_interpolator.process()
+    interpolator.process()
 
-    test_xs = np.linspace(0, 3, 10)
+    test_xs = np.linspace(0.1, 3, 10)
     test_ys = test_xs
     test_zs = test_xs
 
     test_coords = (test_xs, test_ys, test_zs)
 
-    interpolated_grid,_ = gauss_interpolator.interp(test_coords)
+    interpolated_grid = interpolator.interp(test_coords)
 
-    assert np.allclose(interpolated_grid, np.ones((10, 10, 10)))
+    assert np.allclose(interpolated_grid, np.ones((10, 10, 10)), rtol=1e-2)
