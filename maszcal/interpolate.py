8009dc94dee2c0b20aab919961cb5f28b78691e5
dylancromer <dylanmattc@gmail.com>
2019-05-31 12:03:19 -0400

    Added RBF interpolator

--- maszcal/interpolate.py
+++ maszcal/interpolate.py
@@ -1,5 +1,6 @@
 import numpy as np
 import GPy as gpy
+from scipy.interpolate import Rbf
 from maszcal.interp_utils import cartesian_prod, make_flat
 
 
@@ -40,3 +41,25 @@ class GaussInterpolator:
             raise AttributeError(str(err) + "\nGaussian process interpolation not yet optimized.\
                                   You must run GaussInterpolator.process() before \
                                   trying to evaluate the interpolator.")
+
+
+class RbfInterpolator:
+    def __init__(self, coords, grid):
+        self.interp_coords = coords
+        self.interp_grid = grid
+
+    def process(self, function='multiquadric'):
+        point_coords = cartesian_prod(*self.interp_coords).T
+        point_vals = make_flat(self.interp_grid)
+        self.rbfi = Rbf(*point_coords, point_vals, function=function)
+
+    def interp(self, coords):
+        point_coords = cartesian_prod(*coords).T
+        try:
+            return self.rbfi(*point_coords).reshape(*(coord.size for coord in coords))
+
+        except AttributeError as err:
+            raise AttributeError(str(err) + "\nRBF interpolation not yet calculated.\
+                                  You must run RbfInterpolator.process() before \
+                                  trying to evaluate the interpolator.")
+
