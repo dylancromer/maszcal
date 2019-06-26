dylancromer/maszcal

commit 2eeeb0504fcf361753642391b24b99ed7b96f3f5

Started refactoring RbfInterpolator (note specs are failing)

maszcal/interpolate/interpolate.py
@@ -42,9 +42,10 @@ class GaussInterpolator:
 
 
 class RbfInterpolator:
-    def __init__(self, coords, grid, saved_rbf=NoSavedRbf()):
+    def __init__(self, coords, grid, coords_separated=True, saved_rbf=NoSavedRbf()):
         self.interp_coords = coords
         self.interp_grid = grid
+        self.coords_separated = coords_separated
 
         try:
             self.ndim = len(coords)
@@ -55,12 +56,25 @@ class RbfInterpolator:
             self.rbfi = Rbf(saved_rbf=saved_rbf)
 
     def process(self, function='multiquadric'):
-        point_coords = cartesian_prod(*self.interp_coords).T
+        if self.coords_separated:
+            point_coords = cartesian_prod(*self.interp_coords).T
+        else:
+            rs = self.interp_coords[0]
+            params = self.interp_coords[1:]
+            point_coords = cartesian_prod(rs, params).T
+
         point_vals = make_flat(self.interp_grid)
+
         self.rbfi = Rbf(*point_coords, point_vals, function=function)
 
-    def interp(self, coords):
-        point_coords = cartesian_prod(*coords).T
+    def interp(self, coords, coords_separated=True):
+        if self.coords_separated:
+            point_coords = cartesian_prod(*self.interp_coords).T
+        else:
+            rs = self.interp_coords[0]
+            params = self.interp_coords[1:]
+            point_coords = cartesian_prod(rs, params).T
+
         try:
             return self.rbfi(*point_coords).reshape(*(coord.size for coord in coords))
 

commit e0e982048f5b16307421b548e586ff886a51fa6c

Deleted outdated tests, and fixed bug introduced in 2eeeb05

maszcal/interpolate/interpolate.py
@@ -55,7 +55,7 @@ class RbfInterpolator:
         """
         self.interp_coords = coords
         self.interp_grid = grid
-        self.coords_separated = coords_separated
+        self._coords_separated = coords_separated
 
         try:
             self.ndim = len(coords)
@@ -66,7 +66,7 @@ class RbfInterpolator:
             self.rbfi = Rbf(saved_rbf=saved_rbf)
 
     def process(self, function='multiquadric'):
-        if self.coords_separated:
+        if self._coords_separated:
             point_coords = cartesian_prod(*self.interp_coords).T
         else:
             rs = self.interp_coords[0]
@@ -78,8 +78,8 @@ class RbfInterpolator:
         self.rbfi = Rbf(*point_coords, point_vals, function=function)
 
     def interp(self, coords, coords_separated=True):
-        if self.coords_separated:
-            point_coords = cartesian_prod(*self.interp_coords).T
+        if coords_separated:
+            point_coords = cartesian_prod(*coords).T
         else:
             rs = self.interp_coords[0]
             params = self.interp_coords[1]

tests/emulator/LensingEmulator.py
@@ -4,18 +4,6 @@ from maszcal.emulator import LensingEmulator
 
 
 
-def test_emulator_generate_grid():
-    emulator = LensingEmulator()
-
-    rs = np.logspace(-1, 1, 10)
-    cons = np.linspace(2, 5, 5)
-    a_szs = np.linspace(-1, 1, 5)
-    coords = (rs, cons, a_szs)
-
-    grid = emulator.generate_grid(coords)
-    assert grid.shape == (10, 5, 5)
-
-
 def test_lensing_emulator():
     xs = np.linspace(0, 1, 10)
     ys = np.linspace(0, 1, 10)
@@ -23,7 +11,7 @@ def test_lensing_emulator():
     grid = np.ones((10, 10))
 
     emulator = LensingEmulator()
-    emulator.emulate(coords, grid=grid, check_errs=False)
+    emulator.emulate(coords, grid)
 
     xs_subgrid = np.linspace(0, 0.5, 10)
     ys_subgrid = np.linspace(0.5, 1, 10)
@@ -45,7 +33,7 @@ def test_lensing_emulator_higher_dim():
     grid = np.ones((4, 4, 4, 4, 5))
 
     emulator = LensingEmulator()
-    emulator.emulate(coords, grid=grid, check_errs=False)
+    emulator.emulate(coords, grid)
 
     xs_subgrid = np.linspace(0, 0.5, 3)
     ys_subgrid = np.linspace(0.5, 1, 4)

tests/interpolate/GaussInterpolator.py
@@ -1,36 +1,0 @@
-"""
-GaussInterpolator
-
-Interpolates maszcal model grids
-"""
-import numpy as np
-from maszcal.interpolate import GaussInterpolator
-
-
-
-
-length = 3
-xs = np.linspace(0, 3, length)
-ys = np.linspace(0, 3, length)
-zs = np.linspace(0, 3, length)
-coords = (xs, ys, zs)
-grid = np.ones((length, length, length))
-
-gauss_interpolator = GaussInterpolator(coords, grid)
-
-
-"""
-    - it interpolates a constant to be a constant
-"""
-def test_interp_over_constant_input():
-    gauss_interpolator.process()
-
-    test_xs = np.linspace(0, 3, 10)
-    test_ys = test_xs
-    test_zs = test_xs
-
-    test_coords = (test_xs, test_ys, test_zs)
-
-    interpolated_grid,_ = gauss_interpolator.interp(test_coords)
-
-    assert np.allclose(interpolated_grid, np.ones((10, 10, 10)))

tests/interpolate/RbfInterpolator.py
@@ -1,36 +1,0 @@
-"""
-RbfInterpolator
-
-Interpolates maszcal model grids
-"""
-import numpy as np
-from maszcal.interpolate import RbfInterpolator
-
-
-
-
-length = 5
-xs = np.linspace(0, 3, length)
-ys = np.linspace(0, 3, length)
-zs = np.linspace(0, 3, length)
-coords = (xs, ys, zs)
-grid = np.ones((length, length, length))
-
-interpolator = RbfInterpolator(coords, grid)
-
-
-"""
-    - it interpolates a constant to be a constant
-"""
-def test_interp_over_constant_input():
-    interpolator.process()
-
-    test_xs = np.linspace(0.1, 3, 10)
-    test_ys = test_xs
-    test_zs = test_xs
-
-    test_coords = (test_xs, test_ys, test_zs)
-
-    interpolated_grid = interpolator.interp(test_coords)
-
-    assert np.allclose(interpolated_grid, np.ones((10, 10, 10)), rtol=1e-2)

tests/likelihood/GaussianLikelihood.py
@@ -1,9 +1,0 @@
-import numpy as np
-from maszcal.likelihood import GaussianLikelihood
-
-
-
-
-def test_emulator_setup():
-    grid = np.load('data/tests/testgrid.npy')
-    likelihood = GaussianLikelihood(grid=grid)