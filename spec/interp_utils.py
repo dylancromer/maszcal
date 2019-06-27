6f9e479c32eca69ff6603f2a59f9310e1544df09
dylancromer <dylanmattc@gmail.com>
2019-06-26 20:39:34 -0400

    Cleaned up spec

--- spec/interp_utils.py
+++ spec/interp_utils.py
@@ -4,26 +4,27 @@ from maszcal.interp_utils import cartesian_prod, make_flat
 
 
 
-"""
-    - The order of make_flat's output should always align with the ordering given
-    by cartesian product, when the product is interpreted as coordinates for the
-    values of the flattened array
-"""
-def test_orders_equal():
-    xs = np.linspace(1, 10, 10)
-    ys = np.linspace(1, 10, 10)
-    zs = np.linspace(1, 10, 10)
+def describe_cartesian_prod():
 
-    fs = np.random.rand(10, 10, 10)
+    def it_should_always_give_the_same_order_as_flattening():
+        """
+        The order of make_flat's output should always align with the ordering given
+        by cartesian product, when the product is interpreted as coordinates for the
+        values of the flattened array
+        """
+        xs = np.linspace(1, 10, 10)
+        ys = np.linspace(1, 10, 10)
+        zs = np.linspace(1, 10, 10)
 
-    coords = cartesian_prod(xs, ys, zs)
-    flat_func = make_flat(fs)
+        fs = np.random.rand(10, 10, 10)
 
-    coord_inds = []
-    for x in (xs, ys, zs):
-        coord_inds.append(np.linspace(0, x.size - 1, x.size, dtype=int))
+        flat_func = make_flat(fs)
 
-    coord_inds = tuple(cartesian_prod(*coord_inds).T)
-    flat_func_should_be = fs[coord_inds]
+        coord_inds = []
+        for x in (xs, ys, zs):
+            coord_inds.append(np.linspace(0, x.size - 1, x.size, dtype=int))
 
-    assert np.all(flat_func == flat_func_should_be)
+        coord_inds = tuple(cartesian_prod(*coord_inds).T)
+        flat_func_should_be = fs[coord_inds]
+
+        assert np.all(flat_func == flat_func_should_be)
