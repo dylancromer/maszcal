040de58e11ebda1bbc3855fe7c470fbb1792e6b4
dylancromer <dylanmattc@gmail.com>
2019-07-25 15:25:28 -0400

    Refactored likelihood

--- maszcal/likelihood.py
+++ maszcal/likelihood.py
@@ -1,23 +1,27 @@
+from dataclasses import dataclass
 import numpy as np
 
 
 class GaussianLikelihood:
-    def get_theory(self):
-        pass
+    def __init__(self, radii, data, theory_func, covariance):
+        self.radii = radii
+        self.data = data
+        self.theory_func = theory_func
+        self.covariance = covariance
 
     def calc_likelihood(self, params):
         n = params.size
         prefac = 1 / (2*np.pi)**(n/2)
         prefac *= np.sqrt(np.linalg.det(self.covariance))
 
-        theory = self.get_theory(params)
+        theory = self.theory_func(params)
         diff = self.data - theory
         fisher = np.linalg.inv(self.covariance)
 
         return prefac * np.exp(-0.5 * diff.T @ fisher @ diff)
 
     def calc_loglike(self, params):
-        theory = self.get_theory(params)
+        theory = self.theory_func(params)
         diff = self.data - theory
         fisher = np.linalg.inv(self.covariance)
 
--- spec/likelihood/GaussianLikelihood.py
+++ spec/likelihood/GaussianLikelihood.py
@@ -1,40 +1,32 @@
+import pytest
 import numpy as np
 from maszcal.likelihood import GaussianLikelihood
 
 
 def describe_gaussian_likelihood():
 
-    def it_has_a_gaussian_full_likelihood():
-        likelihood = GaussianLikelihood()
-
-        # r, c, a_sz
-        params = np.array((1, 3, 0))
+    @pytest.fixture
+    def likelihood():
         rs, data_dsigs = np.loadtxt('data/test/testdata.csv', delimiter=',').T
         data = rs*data_dsigs
+        covariance = np.identity(rs.size)
 
-        likelihood.radii = rs
-        likelihood.data = data
-        likelihood.covariance = np.identity(rs.size)
+        theory_func = lambda params: data
 
-        likelihood.get_theory = lambda p: data
+        return GaussianLikelihood(radii=rs,
+                                  data=data,
+                                  theory_func=theory_func,
+                                  covariance=covariance)
+
+    def it_has_a_gaussian_full_likelihood(likelihood):
+        params = np.array((3, 0))
 
         like = likelihood.calc_likelihood(params)
 
         assert like == 1 / (2*np.pi)**(params.size/2)
 
-    def it_returns_the_appropriate_log_likelihood():
-        likelihood = GaussianLikelihood()
-
-        # r, c, a_sz
-        params = np.array((1, 3, 0))
-        rs, data_dsigs = np.loadtxt('data/test/testdata.csv', delimiter=',').T
-        data = rs*data_dsigs
-
-        likelihood.radii = rs
-        likelihood.data = data
-        likelihood.covariance = np.identity(rs.size)
-
-        likelihood.get_theory = lambda p: data
+    def it_returns_the_appropriate_log_likelihood(likelihood):
+        params = np.array((3, 0))
 
         loglike = likelihood.calc_loglike(params)
 
