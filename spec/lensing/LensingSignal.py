b47125983aa5a71d58b0e77cd87c31a5367f31fe
dylancromer <dylanmattc@gmail.com>
2019-08-14 11:54:52 -0400

    Added single mass bin to lensing

--- spec/lensing/LensingSignal.py
+++ spec/lensing/LensingSignal.py
@@ -17,19 +17,26 @@ class FakeStackedModel:
     def delta_sigma(self, rs, miscentered=False, units=1):
         return np.ones(12)
 
+class FakeSingleMassModel:
+    def __init__(
+            self,
+            redshift,
+            comoving_radii=True,
+            cosmo_params=defaults.DefaultCosmology(),
+    ):
+        pass
+
+    def delta_sigma(self, rs, mus, concentrations, units=1):
+        return np.ones(13)
+
 
 def describe_lensing_signal():
 
     def describe_init():
 
-        def it_requires_mass_and_redshift():
+        def it_requires_redshifts():
             with pytest.raises(TypeError):
                 LensingSignal()
-
-            mus = np.ones(10)
-            zs = np.ones(5)
-
-            LensingSignal(mus, zs)
 
         def it_accepts_a_selection_func_file(mocker):
             mus = np.ones(10)
@@ -45,7 +52,6 @@ def describe_lensing_signal():
             weights_file = 'test/file/here'
             LensingSignal(mus, zs, lensing_weights_file=weights_file)
 
-
     def describe_stacked_esd():
 
         @pytest.fixture
@@ -54,7 +60,18 @@ def describe_lensing_signal():
 
             mus = np.ones(10)
             zs = np.ones(5)
-            return LensingSignal(mus, zs)
+            return LensingSignal(log_masses=mus, redshifts=zs)
+
+        def it_requires_masses():
+            zs = np.ones(2)
+            lensing_signal = LensingSignal(redshifts=zs)
+
+            rs = np.logspace(-1, 1, 10)
+            params = np.array([[0, 3.01],
+                               [0, 3.02]])
+
+            with pytest.raises(TypeError):
+                esd = lensing_signal.stacked_esd(rs, params)
 
         def it_gives_a_stacked_model_for_the_esd(lensing_signal):
             rs = np.logspace(-1, 1, 10)
@@ -72,3 +89,28 @@ def describe_lensing_signal():
 
             esd = lensing_signal.stacked_esd(rs, params, miscentered=True)
             assert np.all(esd == np.ones(12))
+
+    def describe_single_mass_esd():
+
+        def it_fails_if_there_are_too_many_redshifts():
+            redshifts = np.ones(10)
+            lensing_signal = LensingSignal(redshifts=redshifts)
+
+            rs = np.logspace(-1, 1, 3)
+            params = np.array([[1, 1]])
+
+            with pytest.raises(TypeError):
+                esd = lensing_signal.single_mass_esd(rs, params)
+
+        @pytest.fixture
+        def lensing_signal(mocker):
+            mocker.patch('maszcal.lensing.SingleMassModel', new=FakeSingleMassModel)
+            redshift = np.array([0])
+            return LensingSignal(redshifts=redshift)
+
+        def it_gives_a_single_mass_model_for_the_esd(lensing_signal):
+            rs = np.logspace(-1, 1, 3)
+            params = np.array([[1, 1]])
+
+            esd = lensing_signal.single_mass_esd(rs, params)
+            assert np.all(esd == np.ones(13))
