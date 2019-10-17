222e3be4ed6ea7745d0d050202ce7dd0538864e2
dylancromer <dylanmattc@gmail.com>
2019-10-17 12:53:03 -0400

    Outside-in'd internal structure of BaryonicEmulationErrors

--- maszcal/check.py
+++ maszcal/check.py
@@ -1,3 +1,63 @@
-class EmulationErrors:
-    def get_baryonic_emulation_errors(self, param_mins, param_maxes, sampling='all'):
-        return 1, None
+from dataclasses import dataclass
+import numpy as np
+import astropy.units as u
+import maszcal.defaults as defaults
+import maszcal.emulator
+
+
+@dataclass
+class BaryonicEmulationErrors:
+    radii: np.ndarray
+    log_masses: np.ndarray
+    redshifts: np.ndarray
+    units: u.Quantity = u.Msun/u.pc**2
+    comoving: bool = True
+    delta: int = 200
+    mass_definition: str = 'mean'
+    cosmo_params: object = defaults.DefaultCosmology()
+    selection_func_file: object = defaults.DefaultSelectionFunc()
+    lensing_weights_file: object = defaults.DefaultLensingWeights()
+    emulator_class: object = maszcal.emulator.LensingEmulator
+
+    def _get_params_from_partial(self, param_mins, param_maxes, fixed_params, sampling):
+        pass
+
+    def _get_function_to_interpolate(self, params_to_interpolate):
+        pass
+
+    def _get_emulator(self, params_to_interpolate, function_to_interpolate):
+        emulator = self.emulator_class()
+        emulator.emulate(self.radii, params_to_interpolate, function_to_interpolate)
+        return emulator
+
+    def _get_test_params(self, params_to_interpolate):
+        pass
+
+    def _get_true_values(self, test_params):
+        return 2*np.ones(2)
+
+    def _percent_at_error_level(self, error_level, errors):
+        errors_above_level = errors[np.where(np.abs(errors) > error_level)]
+        return 100*(errors_above_level.size/errors.size)
+
+    def get_emulation_errors(
+            self,
+            param_mins,
+            param_maxes,
+            num_samples,
+            sampling='all',
+            fixed_params=None,
+    ):
+        params_to_interpolate = self._get_params_from_partial(param_mins, param_maxes, fixed_params, sampling)
+        function_to_interpolate = self._get_function_to_interpolate(params_to_interpolate)
+
+        emulator = self._get_emulator(params_to_interpolate, function_to_interpolate)
+
+        test_params = self._get_test_params(params_to_interpolate)
+        emulated_values = emulator.evaluate_on(test_params)
+        true_values = self._get_true_values(test_params)
+
+        rel_errors = np.abs((true_values - emulated_values)/true_values)
+        error_levels = np.logspace(-8, 0, 100)
+        error_fracs = np.array([self._percent_at_error_level(level, rel_errors) for level in error_levels])
+        return error_levels, error_fracs
--- spec/check/EmulationErrors.py
+++ spec/check/EmulationErrors.py
@@ -1,13 +1,27 @@
 import numpy as np
 import pytest
 import maszcal.check as check
+from maszcal.cosmology import CosmoParams
+
+
+class PretendEmulator:
+    def emulate(self, radii, params, func_vals):
+        pass
+
+    def evaluate_on(self, params):
+        return np.ones(2)
 
 
 def describe_emulation_errors():
 
     @pytest.fixture
     def emulation_errors():
-        return check.EmulationErrors()
+        rs = np.logspace(-1, 1, 5)
+        mus = np.linspace(np.log(1e14), np.log(1e16), 6)
+        zs = np.linspace(0, 1, 6)
+
+        emulator_class = PretendEmulator
+        return check.BaryonicEmulationErrors(rs, mus, zs, emulator_class=emulator_class)
 
     def it_produces_a_number_versus_error_level_curve(emulation_errors):
         CON_MIN = 1
@@ -18,10 +32,64 @@ def describe_emulation_errors():
         param_mins = np.array([CON_MIN, A_SZ_MIN])
         param_maxes = np.array([CON_MAX, A_SZ_MAX])
 
-        errors, error_fracs = emulation_errors.get_baryonic_emulation_errors(
+        fixed_params = {'alpha':0.88, 'beta':3.8, 'gamma':0.2}
+
+        num_samples = 100
+
+        error_levels, error_fracs = emulation_errors.get_emulation_errors(
             param_mins,
             param_maxes,
-            sampling='lh'
+            num_samples,
+            sampling='lh',
+            fixed_params=fixed_params,
         )
 
-        assert errors > 0
+        assert np.all(error_levels > 0)
+
+    def describe_init():
+
+        def it_requires_redshifts():
+            rs = np.logspace(-1, 1, 5)
+            mus = np.linspace(np.log(1e14), np.log(1e16), 6)
+            zs = np.linspace(0, 1, 6)
+            with pytest.raises(TypeError):
+                check.BaryonicEmulationErrors(radii=rs, log_masses=mus)
+
+        def it_accepts_a_selection_func_file(mocker):
+            rs = np.logspace(-1, 1, 5)
+            mus = np.ones(10)
+            zs = np.ones(5)
+            sel_func_file = 'test/file/here'
+            emu_errs = check.BaryonicEmulationErrors(rs, mus, zs, selection_func_file=sel_func_file)
+
+            assert emu_errs.selection_func_file == sel_func_file
+
+        def it_accepts_a_weights_file(mocker):
+            rs = np.logspace(-1, 1, 5)
+            mus = np.ones(10)
+            zs = np.ones(5)
+            weights_file = 'test/file/here'
+            emu_errs = check.BaryonicEmulationErrors(rs, mus, zs, lensing_weights_file=weights_file)
+
+            assert emu_errs.lensing_weights_file == weights_file
+
+        def it_allows_a_different_mass_definition(mocker):
+            rs = np.logspace(-1, 1, 5)
+            mus = np.ones(10)
+            zs = np.ones(5)
+
+            delta = 500
+            mass_definition = 'crit'
+
+            emu_errs = check.BaryonicEmulationErrors(rs, mus, zs, delta=delta, mass_definition=mass_definition)
+            assert emu_errs.mass_definition == mass_definition
+
+        def it_can_use_a_different_cosmology(mocker):
+            rs = np.logspace(-1, 1, 5)
+            mus = np.ones(10)
+            zs = np.ones(5)
+
+            cosmo = CosmoParams(neutrino_mass_sum=1)
+            emu_errs = check.BaryonicEmulationErrors(rs, mus, zs, cosmo_params=cosmo)
+
+            assert emu_errs.cosmo_params == cosmo
