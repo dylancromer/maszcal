4ceddee3fc0b0a4ae5b107f830858a9fde27a0a2
dylancromer <dylanmattc@gmail.com>
2019-10-31 11:43:20 -0400

    Removed fitting utilities

--- maszcal/fitting/__init__.py
+++
@@ -1,6 +0,0 @@
-from .mcmc import *
-from .max_likelihood import *
-from . import likelihood
-
-
-__all__ = [s for s in dir() if not s.startswith('_')]
--- spec/fitting/McmcFitter.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import numpy as np
-
-
-def describe_mcmc_fitter():
-
-    def it_works():
-        pass
