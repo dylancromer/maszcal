372c4a4c2d1629c383a370d8177aad216bcde8da
Dylan Cromer <dylan@dylancromer.com>
2020-02-20 10:37:46 -0500

    Drove out consistency checks in data templates

--- maszcal/data/templates.py
+++ maszcal/data/templates.py
@@ -7,3 +7,12 @@ class WeakLensingData:
     radii: np.ndarray
     redshifts: np.ndarray
     wl_signals: np.ndarray
+
+    def __post_init__(self):
+        if not self._data_are_consistent(self.radii, self.redshifts, self.wl_signals):
+            raise ValueError('wl_signals must have shape radii.shape + redshifts.shape + (n,)')
+
+    def _data_are_consistent(self, radii, redshifts, wl_signals):
+        condition_1 = wl_signals.shape[:2] == radii.shape + redshifts.shape
+        condition_2 = wl_signals.ndim <= 3
+        return condition_1 and condition_2
--- spec/data/templates.py
+++ spec/data/templates.py
@@ -5,16 +5,60 @@ from maszcal.data.templates import WeakLensingData
 
 def describe_weak_lensing_data():
 
-    @pytest.fixture
-    def wl_data():
-        return WeakLensingData(
-            radii = np.logspace(-1, 1, 10),
-            redshifts = np.linspace(0, 1, 5),
-            wl_signals = np.ones((10, 5, 3)),
-        )
+    def describe___init__():
 
-    def it_must_be_inited_with_data(wl_data):
-        assert wl_data.radii is not None
+        @pytest.fixture
+        def wl_data():
+            return WeakLensingData(
+                radii=np.logspace(-1, 1, 10),
+                redshifts=np.linspace(0, 1, 5),
+                wl_signals=np.ones((10, 5, 3)),
+            )
 
-        with pytest.raises(TypeError):
-            WeakLensingData()
+        def it_must_be_inited_with_data(wl_data):
+            assert wl_data.radii is not None
+
+            with pytest.raises(TypeError):
+                WeakLensingData()
+
+    def describe__data_are_consistent():
+
+        @pytest.fixture
+        def mismatched_data():
+            num_radii = 3
+            num_redshifts = 2
+            wrong_num_radii = 4
+            wrong_num_redshifts = 5
+
+            radii = np.logspace(-1, 1, num_radii)
+            redshifts = np.linspace(0, 1, num_redshifts)
+
+            wl_wrong_radii = np.ones((wrong_num_radii, num_redshifts))
+            wl_wrong_redshifts = np.ones((num_radii, wrong_num_redshifts))
+            wl_wrong_both = np.ones((wrong_num_radii, wrong_num_redshifts))
+
+            return radii, redshifts, wl_wrong_radii, wl_wrong_redshifts, wl_wrong_both
+
+        def it_makes_sure_the_data_shapes_are_consistent(mismatched_data):
+            radii, redshifts, wl_wrong_radii, wl_wrong_redshifts, wl_wrong_both = mismatched_data
+
+            with pytest.raises(ValueError):
+                WeakLensingData(
+                    radii=radii,
+                    redshifts=redshifts,
+                    wl_signals=wl_wrong_radii,
+                )
+
+            with pytest.raises(ValueError):
+                WeakLensingData(
+                    radii=radii,
+                    redshifts=redshifts,
+                    wl_signals=wl_wrong_redshifts,
+                )
+
+            with pytest.raises(ValueError):
+                WeakLensingData(
+                    radii=radii,
+                    redshifts=redshifts,
+                    wl_signals=wl_wrong_both,
+                )
