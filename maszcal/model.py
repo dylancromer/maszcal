f637079062b18159bcc824cf956b173f51ebb433
Dylan Cromer <dylan@dylancromer.com>
2020-02-28 13:35:34 -0500

    Drove out maszcal.model.NfwSingleMass partially, current implementation is slimed

---
+++ maszcal/model.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+class NfwSingleMass:
+    def esd(self, rs, params):
+        return np.ones(4, 1)
--- spec/model/NfwSingleMass.py
+++ spec/model/NfwSingleMass.py
@@ -1,7 +1,48 @@
 import pytest
+import numpy as np
+import maszcal.model
+import maszcal.data.templates
 
 
 def describe_nfw_single_mass():
 
-    def it_does_stuff():
-        pass
+    def describe_esd():
+
+        @pytest.fixture
+        def nfw_model():
+            return maszcal.model.NfwSingleMass()
+
+        def it_calculates_an_esd(nfw_model):
+            rs = np.logspace(-1, 1, 4)
+            params = np.array([3, 1e14])
+
+            esd = nfw_model.esd(rs, params)
+
+            assert np.all(esd > 0)
+
+    def describe_get_best_fit():
+
+        @pytest.fixture
+        def nfw_model():
+            return maszcal.model.NfwSingleMass()
+
+        @pytest.fixture
+        def data(nfw_model):
+            rs = np.logspace(-1, 1, 4)
+            zs = np.zeros(1)
+            params = np.array([3, 1e14])
+
+            esd = nfw_model.esd(rs, params)
+            covariance = np.identity(4)
+
+            return maszcal.data.templates.WeakLensingData(
+                radii=rs,
+                redshifts=zs,
+                wl_signals=esd[..., None],
+                covariances=covariance[..., None],
+            )
+
+        def it_gets_the_best_fit_for_the_input_data(nfw_model, data):
+            best_fit = nfw_model.get_best_fit(data, data.redshifts)
+
+            assert np.allclose(best_fit, np.array([3, 1e14]))
