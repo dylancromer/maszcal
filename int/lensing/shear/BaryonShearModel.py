aa60bb9d315e7315c7bc5fc9ce9780621746cbc5
Dylan Cromer <dylan@dylancromer.com>
2020-03-17 15:49:02 -0400

    Updated integration test to check that NFW params reduce the GNFW to an NFW

--- int/lensing/shear/BaryonShearModel.py
+++ int/lensing/shear/BaryonShearModel.py
@@ -12,7 +12,7 @@ sns.set(style='whitegrid', font_scale=1.5, rc={"lines.linewidth": 2,'lines.marke
 from maszcal.lensing.shear import BaryonShearModel
 
 
-def describe_gaussian_baryonic_model():
+def describe_gnfw_baryonic_model():
 
     def describe_gnfw_rho():
 
@@ -76,6 +76,44 @@ def describe_gaussian_baryonic_model():
             mus = np.linspace(np.log(1e14), np.log(1e15), 30)
             zs = np.linspace(0, 1, 20)
             return BaryonShearModel(mus, zs, units=u.Msun/u.pc**2)
+
+        @pytest.fixture
+        def baryon_model_():
+            mus = np.linspace(np.log(1e14), np.log(1e15), 30)
+            zs = np.linspace(0, 1, 20)
+            model = BaryonShearModel(mus, zs, units=u.Msun/u.pc**2)
+            model.CORE_RADIUS = 1
+            return model
+
+        def it_can_recreate_an_nfw_model(baryon_model_):
+            radii = np.logspace(-1, 1, 30)
+            mus = np.log(1e14)*np.ones(1)
+            cons = 2*np.ones(1)
+            alphas = np.ones(1)
+            betas = 3*np.ones(1)
+            gammas = np.ones(1)
+
+            assert baryon_model_.CORE_RADIUS == 1
+
+            esds_bary = baryon_model_.delta_sigma_bary(radii,
+                                                       mus,
+                                                       cons,
+                                                       alphas,
+                                                       betas,
+                                                       gammas)/baryon_model_.baryon_frac
+
+            esds_total = baryon_model_.delta_sigma_total(radii,
+                                                         mus,
+                                                         cons,
+                                                         alphas,
+                                                         betas,
+                                                         gammas)
+
+            esds_nfw = baryon_model_.delta_sigma_cdm(radii,
+                                                     mus,
+                                                     cons)/(1-baryon_model_.baryon_frac)
+
+            assert np.allclose(esds_bary, esds_nfw)
 
         def the_plots_look_right(baryon_model):
             radii = np.logspace(-1, 1, 30)
