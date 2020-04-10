bbf227f40795cc7e9208732fe102bbbf64d904d2
Dylan Cromer <dylan@dylancromer.com>
2020-04-10 11:04:44 -0400

    Added twohalo emulator

--- spec/twohalo.py
+++ spec/twohalo.py
@@ -3,6 +3,7 @@ import pytest
 import numpy as np
 import maszcal.twohalo
 import maszcal.cosmology
+import maszcal.interp_utils
 
 
 @dataclass
@@ -38,5 +39,49 @@ def describe_TwoHaloShearModel():
         esds = two_halo_model.esd(rs, mus, zs)
 
         assert np.all(esds >= 0)
+        assert not np.any(np.isnan(esds))
+        assert esds.shape == mus.shape + zs.shape + rs.shape
+
+
+@dataclass
+class FakeTwoHaloShearModel:
+    cosmo_params: object
+    units: object
+    delta: int
+    mass_definition: str
+    comoving: bool
+    matter_power_class: object
+
+    def _esd_radial_shape(self, rs, zs):
+        return np.ones(rs.shape + zs.shape)
+
+    def _bias(self, mus, zs):
+        return np.ones(mus.shape + zs.shape)
+
+
+def describe_EmulatedTwoHaloShear():
+
+    @pytest.fixture
+    def emulated_model():
+        zs = np.linspace(0, 1, 8)
+        rs = np.logspace(-1, 1, 8)
+
+        emu = maszcal.twohalo.EmulatedTwoHaloShear(
+            rs,
+            zs,
+            cosmo_params=maszcal.cosmology.CosmoParams(),
+            two_halo_shear_class=FakeTwoHaloShearModel,
+        )
+        emu.process()
+        return emu
+
+    def it_emulated_two_halo_esds(emulated_model):
+        zs = np.linspace(0.1, 0.9, 10)
+        mus = np.linspace(32, 33, 10)
+        rs = np.logspace(np.log10(0.2), np.log10(9), 10)
+
+        esds = emulated_model.esd(rs, mus, zs)
+
+        assert np.allclose(esds, 1, rtol=1e-1)
         assert not np.any(np.isnan(esds))
         assert esds.shape == mus.shape + zs.shape + rs.shape
