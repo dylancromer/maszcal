8a0d893b16fe55bb9f9e7466f89d8e2126e4d4e2
Dylan Cromer <dylan@dylancromer.com>
2020-05-07 10:06:02 -0400

    Removed maszcal.check, updated gitignore, renamed emulator module to emulate

--- spec/emulate.py
+++ spec/emulate.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-import maszcal.emulator
+import maszcal.emulate
 import maszcal.interpolate
 import pality
 
@@ -14,7 +14,7 @@ def describe_LensingPca():
             return 2*np.random.randn(10, 5) + 1
 
         def it_normalizes_the_lensing_data(data):
-            standardized_data =  maszcal.emulator.LensingPca.standardize(data)
+            standardized_data =  maszcal.emulate.LensingPca.standardize(data)
             assert np.allclose(standardized_data.mean(axis=-1), 0)
             assert np.allclose(standardized_data.std(axis=-1), 1)
 
@@ -25,7 +25,7 @@ def describe_LensingPca():
             return np.random.randn(10, 5)
 
         def it_retrieves_a_pca_from_pality(data):
-            pca = maszcal.emulator.LensingPca.get_pca(data)
+            pca = maszcal.emulate.LensingPca.get_pca(data)
             assert isinstance(pca, pality.PcData)
 
     @pytest.fixture
@@ -33,7 +33,7 @@ def describe_LensingPca():
         return 2*np.random.randn(10, 5) + 1
 
     def it_returns_a_pca_of_the_standardized_data(data):
-        pca = maszcal.emulator.LensingPca.create(data)
+        pca = maszcal.emulate.LensingPca.create(data)
         reconstructed_data = pca.basis_vectors @ pca.weights
         assert np.allclose(reconstructed_data.mean(axis=-1), 0)
         assert np.allclose(reconstructed_data.std(axis=-1), 1)
@@ -51,10 +51,10 @@ def describe_PcaEmulator():
 
     @pytest.fixture
     def pca(data):
-        return maszcal.emulator.LensingPca.create(data)
+        return maszcal.emulate.LensingPca.create(data)
 
     def it_emulates_a_pca_to_reproduce_data(coords, data, pca):
-        emulator = maszcal.emulator.PcaEmulator(
+        emulator = maszcal.emulate.PcaEmulator(
             mean=data.mean(axis=-1),
             std_dev=data.std(axis=-1),
             coords=coords,
