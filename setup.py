5e709a059c9a88cb2b9b12bcfcb5252a2c2f1be0
Dylan Cromer <dylan@dylancromer.com>
2020-06-02 11:31:29 -0400

    Started cython project structure

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
-from distutils.core import setup
-setup(name='maszcal',
-      version='0.1',
-      py_modules=['maszcal'],
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+import numpy as np
+
+
+extension = Extension('cmaszcal', ['cmaszcal/*.pyx'], include_dirs=[np.get_include()])
+
+
+setup(
+    name='maszcal',
+    version='0.1',
+    py_modules=['maszcal'],
+    ext_modules = cythonize(extension, include_path=[np.get_include()]),
+    include_dirs=[np.get_include()],
 )
