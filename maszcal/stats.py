c06e23808b025cd273396ba229cc12ab5b5f5002
Dylan Cromer <dylan@dylancromer.com>
2020-08-21 12:59:33 -0400

    ENH: Added lognormal SZ mass distribution

--- maszcal/stats.py
+++ maszcal/stats.py
@@ -8,3 +8,12 @@ class MiscenteringDistributions:
         scale = maszcal.mathutils.atleast_kd(scale, x.ndim+1, append_dims=False)
         x = x[..., None]
         return (x/scale**2) * np.exp(-(x/scale)**2 / 2)
+
+
+class SzMassDistributions:
+    @staticmethod
+    def lognormal_dist(mu, mu_sz, a_sz, b_sz, scatter):
+        mu, mu_sz, a_sz, b_sz, scatter = maszcal.mathutils.expand_parameter_dims(mu, mu_sz, a_sz, b_sz, scatter)
+        prefac = 1 / (np.sqrt(2*np.pi) * scatter)
+        diff = mu_sz - b_sz*mu - a_sz
+        return prefac * np.exp(-diff**2/(2*scatter**2))
